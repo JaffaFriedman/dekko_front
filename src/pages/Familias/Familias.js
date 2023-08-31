7edd0b962e00c0bb8e8bbd35e8ccb02733e52044
JaffaFriedman <jafriedman.dwfsc4@bootcampudd.cl>
2023-08-31 17:19:31 -0400

    Se deja la familia como tabla del programa

--- src/pages/Familias/Familias.js
+++ src/pages/Familias/Familias.js
@@ -1,12 +1,37 @@
-import { useContext , useState ,useEffect} from 'react'
+import { useContext  } from 'react'
 import { GlobalContext } from '../../context/global/globalContext'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { Container } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+
+const tablaFamilias = [
+  {
+    familia: 'MURALES A MEDIDA',
+    width: '25%',
+    url: 'https://static.wixstatic.com/media/12b4f4_5e8d8cd870994115a76f5d56c0235043~mv2.jpg/v1/fill/w_297,h_223,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/12b4f4_5e8d8cd870994115a76f5d56c0235043~mv2.jpg',
+    link: '/Papeles',
+    mensaje:
+      'Este producto se fabrica a medida del muro, debes elegir la textura del papel e ingresar las medidas para calcular el precio.'
+  },
+  {
+    familia: 'PAPEL MURAL',
+    width: '25%',
+    url: 'https://static.wixstatic.com/media/12b4f4_aedb4e4173604a86b9df7ed655739803~mv2.jpg/v1/fill/w_758,h_758,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/12b4f4_aedb4e4173604a86b9df7ed655739803~mv2.jpg',
+    link: '',
+    mensaje: ''
+  },
+  {
+    familia: 'CORTINAS Y TOLDOS',
+    width: '25%',
+    url: 'https://static.wixstatic.com/media/12b4f4_d2bf085eb79645feb8503b692b369810~mv2.jpg/v1/fill/w_318,h_226,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/12b4f4_d2bf085eb79645feb8503b692b369810~mv2.jpg',
+    link: '/Cortinas',
+    mensaje:
+      'Este producto se fabrica a medida, debes elegir la tela o color e ingresar las medidas de la ventana para calcular el precio'
+  }
+]
 
 export default function Familias () {
   const {
@@ -15,31 +40,9 @@ export default function Familias () {
     Image,
     ImageBackdrop,
     ImageMarked,
-    setFamilia,
-    BACKEND_URL
+    setFamilia 
   } = useContext(GlobalContext)
 
-  const [tablaFamilias, setTablaFamilias] = useState([])
-
-  const api = axios.create({
-    baseURL: BACKEND_URL,
-    timeout: 5000,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  const recuperaFamilias = async () => {
-    try {
-      const { data } = await api.get('/familias')
-      setTablaFamilias(data.info)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    recuperaFamilias()
-  })
 
   const navigate = useNavigate()
   const handleFamilia = p => {
